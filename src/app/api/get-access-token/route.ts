9977a3027c56125611185bf5f1bae735165c4b3d
ivespf12 <ives@koalitytech.com>
2025-09-17 19:48:11 -0300

    Separating access token and client credentials routes

--- src/app/api/get-access-token/route.ts
+++ src/app/api/get-access-token/route.ts
@@ -1,9 +1,31 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
 
-export async function GET() {
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const { code, clientId }: { code: string; clientId: string } = body;
+
+  if (!code) {
+    return NextResponse.json(
+      {
+        error: "No code provided",
+      },
+      { status: 400 }
+    );
+  }
+
+  if (!clientId) {
+    return NextResponse.json(
+      {
+        error: "No client id provided",
+      },
+      { status: 400 }
+    );
+  }
+
   const client_id = process.env.SPOTIFY_CLIENT_ID;
-  const client_secret = process.env.SPOTIFY_SECRET_ID;
+  const callback_url = process.env.CALLBACK_URL;
 
   if (!client_id) {
     return NextResponse.json(
@@ -14,37 +36,44 @@ export async function GET() {
     );
   }
 
-  if (!client_secret) {
+  if (!callback_url) {
     return NextResponse.json(
       {
-        error: "No client secret detected",
+        error: "No callback URL detected",
       },
       { status: 400 }
     );
   }
 
-  const authHeader = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
-
   try {
-    const response = await axios.post(
-      "https://accounts.spotify.com/api/token",
-      new URLSearchParams({ grant_type: "client_credentials" }), // body
-      {
-        headers: {
-          Authorization: `Basic ${authHeader}`,
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
+    const verifier = localStorage.getItem("verifier");
 
-    const token = response.data.access_token;
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", process.env.NEXT_PUBLIC_CALLBACK_URL || "");
+    params.append("code_verifier", verifier!);
 
-    return NextResponse.json({ accessToken: token });
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: params,
+    });
+
+    const { access_token } = await result.json();
+
+    return NextResponse.json({ accessToken: access_token });
   } catch (error: any) {
-    console.error("Error getting Spotify token:", error.response?.data || error.message);
+    console.error(
+      "Error getting Spotify token:",
+      error.response?.data || error.message
+    );
     return NextResponse.json(
       {
-        error: `Error getting Spotify token: ${error.response?.data || error.message}`
+        error: `Error getting Spotify token: ${
+          error.response?.data || error.message
+        }`,
       },
       { status: 500 }
     );
